70e521a59d5697551404f899377792fd190b23a8
galo-novoa <galocnovoa@gmail.com>
2025-09-01 01:41:36 -0300

    Base

---
+++ src/main/java/com/galonovoa/mercado/MercadoWebBackApplication.java
@@ -0,0 +1,13 @@
+package com.galonovoa.mercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoWebBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoWebBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galonovoa/mercado/MercadoWebBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galonovoa.mercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoWebBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
