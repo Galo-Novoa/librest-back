2454f893da091840ce18b12fb10cb92fdfe4e52d
Galo Novoa <galocnovoa@gmail.com>
2025-10-12 00:31:19 -0300

    Carga de credenciales de la base de datos desde .env

--- src/main/java/com/galonovoa/mercado/MercadoWebBackApplication.java
+++ src/main/java/com/galonovoa/mercado/MercadoWebBackApplication.java
@@ -2,12 +2,21 @@ package com.galonovoa.mercado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 @SpringBootApplication
 public class MercadoWebBackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MercadoWebBackApplication.class, args);
-	}
+public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+        SpringApplication.run(MercadoWebBackApplication.class, args);
+    }
 
 }
