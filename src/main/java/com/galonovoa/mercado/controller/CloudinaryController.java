cdcc55b9254e29198c86613336a4d303c64f056d
Galo Novoa <galocnovoa@gmail.com>
2025-10-13 00:21:46 -0300

    Fix: No cargaban las credenciales de Cloudinary

--- src/main/java/com/galonovoa/mercado/controller/CloudinaryController.java
+++ src/main/java/com/galonovoa/mercado/controller/CloudinaryController.java
@@ -4,20 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 import org.apache.commons.codec.digest.DigestUtils;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @RestController
 public class CloudinaryController {
 
-    private final String cloudinaryApiKey = System.getenv("CLOUDINARY_API_KEY");
-    private final String cloudinaryApiSecret = System.getenv("CLOUDINARY_API_SECRET");
-    private final String cloudinaryCloudName = System.getenv("CLOUDINARY_CLOUD_NAME");
+    private final Dotenv dotenv = Dotenv.load();
+
+    private final String cloudinaryApiKey = dotenv.get("CLOUDINARY_API_KEY");
+    private final String cloudinaryApiSecret = dotenv.get("CLOUDINARY_API_SECRET");
+    private final String cloudinaryCloudName = dotenv.get("CLOUDINARY_CLOUD_NAME");
 
     @GetMapping("/cloudinary/signature")
     public Map<String, Object> getSignature() {
         long timestamp = System.currentTimeMillis() / 1000;
-
         String toSign = "timestamp=" + timestamp + cloudinaryApiSecret;
-        String signature = DigestUtils.sha1Hex(toSign);
+        String signature = org.apache.commons.codec.digest.DigestUtils.sha1Hex(toSign);
 
         return Map.of(
             "signature", signature,
