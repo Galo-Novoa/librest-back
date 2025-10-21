341eef9181c7583364fa0ef3a7637d08a0d21643
Galo Novoa <galocnovoa@gmail.com>
2025-10-21 01:45:34 -0300

    Fix: Puerto 5174 añadido a la configuración de CORS.

--- src/main/java/com/galonovoa/librest/config/CorsConfig.java
+++ src/main/java/com/galonovoa/librest/config/CorsConfig.java
@@ -15,9 +15,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "http://localhost:5174")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
